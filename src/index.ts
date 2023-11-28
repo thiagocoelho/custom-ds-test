05d67b97583d7d08723478ea5cc19d7c41d22d66
Thiago Coelho <thiago.coelho@olos.com.br>
2023-11-28 11:34:43 -0300

    removed TestComponent

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,8 @@ export * from './ThemeRegistry';
 export * from './providers';
 import { themes } from './styles';
 
-import TestComponent from "./TestComponent";
 import { Button, Accordion } from "@mui/material";
 import { NormaProvider } from "./providers/NormaProvider";
 
 
-export { TestComponent, Button, Accordion, NormaProvider, themes};
+export { Button, Accordion, NormaProvider, themes};
