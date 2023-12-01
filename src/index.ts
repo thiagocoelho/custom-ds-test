4bc81acc6c7cfa2c55d6838375923b86a8ef90d8
Thiago Coelho <thiago.coelho@olos.com.br>
2023-12-01 10:50:39 -0300

    added all material components used by olos-analytics

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,73 @@
 'use client';
 
 import { themes } from './themes/defaultTheme';
-export * from './providers';
-
-// import Button from "./components/Button";
-import TextField from "./components/TextField";
-import { Accordion } from "@mui/material";
-import { NormaProvider } from "./providers/NormaProvider";
-
 import Button from './components/Button';
+import TextField from "./components/TextField";
 
-export { Button, Accordion, TextField, NormaProvider, themes};
+import type SelectChangeEvent from '@mui/material';
+
+import { 
+  Accordion,
+  IconButton, 
+  SvgIcon, 
+  Breadcrumbs, 
+  Typography, 
+  LinearProgress, 
+  Chip, 
+  Card, 
+  Box, 
+  Grid, 
+  Paper, 
+  Select, 
+  FormControl, 
+  ListItem, 
+  ListItemText, 
+  ListItemAvatar, 
+  Avatar, 
+  InputLabel, 
+  MenuItem, 
+  Checkbox, 
+  InputAdornment, 
+  Table, 
+  TableContainer, 
+  TableHead, 
+  TableRow, 
+  TableCell, 
+  TableBody
+} from "@mui/material";
+
+export { 
+  Button, 
+  Accordion, 
+  TextField, 
+  themes,
+  IconButton, 
+  SvgIcon, 
+  Breadcrumbs, 
+  Typography, 
+  LinearProgress, 
+  Chip, 
+  Card, 
+  Box, 
+  Grid, 
+  Paper, 
+  Select, 
+  FormControl, 
+  ListItem, 
+  ListItemText, 
+  ListItemAvatar, 
+  Avatar, 
+  InputLabel, 
+  MenuItem, 
+  Checkbox, 
+  InputAdornment, 
+  Table, 
+  TableContainer, 
+  TableHead, 
+  TableRow, 
+  TableCell, 
+  TableBody,
+  SelectChangeEvent
+};
+
+export * from './providers';
